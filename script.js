48f4cd9cd3000fa7ee3dd6c1ec59e3ca85705dcb
zetame <66808008+zetame@users.noreply.github.com>
2020-08-27 23:27:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+
+function play1() {
+    var audio = document.getElementById("audio1");
+    audio.play();
+  }
+
+  function play2() {
+    var audio = document.getElementById("audio2");
+    audio.play();
+  }
+
+  function play3() {
+    var audio = document.getElementById("audio3");
+    audio.play();
+  }
+
 function Random1 () {
     var myrandom=Math.round(Math.random()*2)
 
     if (myrandom==0)    { 
         var pic = "https://www.clker.com/cliparts/p/G/a/s/t/g/rock-md.png"
-        document.getElementById('player1').src = pic.replace('50x50', '225x90');
-        alert("ROCK");
+        document.getElementById('player1').src = pic.replace('50x50', '225x90')
+        play1();
         }
     else if (myrandom==1)
        { var pic = "https://www.vhv.rs/dpng/d/490-4906131_rock-paper-scissors-clipart-rock-paper-scissors-png.png"
-       document.getElementById('player1').src = pic.replace('50x50', '225x90');
-        alert("PAPER");}
+       document.getElementById('player1').src = pic.replace('50x50', '225x90')
+       play2();
+       }
     else if (myrandom==2)
        { var pic = "https://www.kindpng.com/picc/m/502-5025794_rock-paper-scissors-clipart-hd-png-download.png"
-       document.getElementById('player1').src = pic.replace('50x50', '225x90');
-        alert("SCISSOR");}
+       document.getElementById('player1').src = pic.replace('50x50', '225x90')
+       play3();
+        }
     
 }
 
@@ -22,17 +40,19 @@ function Random2 () {
 
     if (myrandom==0)    { 
         var pic = "https://www.clker.com/cliparts/p/G/a/s/t/g/rock-md.png"
-        document.getElementById('player2').src = pic.replace('50x50', '225x90');
-        alert("ROCK");
+        document.getElementById('player2').src = pic.replace('50x50', '225x90')
+        play1();
+        
         }
     else if (myrandom==1)
        { var pic = "https://www.vhv.rs/dpng/d/490-4906131_rock-paper-scissors-clipart-rock-paper-scissors-png.png"
-       document.getElementById('player2').src = pic.replace('50x50', '225x90');
-        alert("PAPER");}
+       document.getElementById('player2').src = pic.replace('50x50', '225x90')
+       play2();
+        }
     else if (myrandom==2)
        { var pic = "https://www.kindpng.com/picc/m/502-5025794_rock-paper-scissors-clipart-hd-png-download.png"
-       document.getElementById('player2').src = pic.replace('50x50', '225x90');
-        alert("SCISSOR");}
+       document.getElementById('player2').src = pic.replace('50x50', '225x90')
+       play3();
+        }
     
 }
-
