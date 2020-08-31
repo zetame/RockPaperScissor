bd59bbf47e8586899095b4f7627f2bd4cfcb6e72
zetame <66808008+zetame@users.noreply.github.com>
2020-08-30 21:38:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function play0() {
+  var audio = document.getElementById("audio0");
+  audio.play();
+}
 
 function play1() {
     var audio = document.getElementById("audio1");
@@ -21,16 +25,19 @@ function Random1 () {
         var pic = "https://www.clker.com/cliparts/p/G/a/s/t/g/rock-md.png"
         document.getElementById('player1').src = pic.replace('50x50', '225x90')
         play1();
+        document.getElementById("button1").innerHTML="ROCK";
         }
     else if (myrandom==1)
        { var pic = "https://www.vhv.rs/dpng/d/490-4906131_rock-paper-scissors-clipart-rock-paper-scissors-png.png"
        document.getElementById('player1').src = pic.replace('50x50', '225x90')
        play2();
+       document.getElementById("button1").innerHTML="PAPER";
        }
     else if (myrandom==2)
        { var pic = "https://www.kindpng.com/picc/m/502-5025794_rock-paper-scissors-clipart-hd-png-download.png"
        document.getElementById('player1').src = pic.replace('50x50', '225x90')
        play3();
+       document.getElementById("button1").innerHTML="SCISSOR";
         }
     
 }
@@ -42,17 +49,20 @@ function Random2 () {
         var pic = "https://www.clker.com/cliparts/p/G/a/s/t/g/rock-md.png"
         document.getElementById('player2').src = pic.replace('50x50', '225x90')
         play1();
+        document.getElementById("button2").innerHTML="ROCK";
         
         }
     else if (myrandom==1)
        { var pic = "https://www.vhv.rs/dpng/d/490-4906131_rock-paper-scissors-clipart-rock-paper-scissors-png.png"
        document.getElementById('player2').src = pic.replace('50x50', '225x90')
        play2();
+       document.getElementById("button2").innerHTML="PAPER";
         }
     else if (myrandom==2)
        { var pic = "https://www.kindpng.com/picc/m/502-5025794_rock-paper-scissors-clipart-hd-png-download.png"
        document.getElementById('player2').src = pic.replace('50x50', '225x90')
        play3();
+       document.getElementById("button2").innerHTML="SCISSOR";
         }
     
 }
